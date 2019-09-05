99f596cbd3a197b3a186de705d57a5f955c21302
tamastorok <torok.tomi@gmail.com>
2019-09-05 21:06:29 +0200

    Add files via upload

---
+++ tourModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+//Mongoose schema - describing data and adding validators
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price']
+  }
+});
+
+//Create module
+const Tour = mongoose.model('Tour', tourSchema);
+module.exports = Tour;
